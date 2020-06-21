11343f3ac67bde73939fe7a8f4009e213a8236f1
x0y14 <ya01ma08@gmail.com>
2020-06-22 01:21:52 +0900

    Initial Commit

---
+++ ios-app-animation-01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-app-animation-01
+//
+//  Created by yuhei yamauchi on 2020/06/22.
+//  Copyright © 2020 x0y14. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-app-animation-01Tests/ios_app_animation_01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_app_animation_01Tests.swift
+//  ios-app-animation-01Tests
+//
+//  Created by yuhei yamauchi on 2020/06/22.
+//  Copyright © 2020 x0y14. All rights reserved.
+//
+
+import XCTest
+@testable import ios_app_animation_01
+
+class ios_app_animation_01Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
